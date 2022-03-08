13b2850dc4bdb65e319c4d9e8f0a14f1a6e63317
tapia1013 <manbearpig1013@gmail.com>
2022-03-08 13:03:59 -0800

    added bcrypt

---
+++ backend/data/users.js
@@ -0,0 +1,19 @@
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: xxxx,
+    isAdmin: true
+  },
+  {
+    name: 'Cookie',
+    email: 'cookie@example.com',
+    password: xxxx,
+    isAdmin: true
+  },
+  {
+    name: 'Nye',
+    email: 'nye@example.com',
+    password: xxxx,
+  },
+]
